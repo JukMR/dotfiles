9481b0cd8b6376815c1d4a508e95a3896f52ffbf
Julian <julianmr97@gmail.com>
2019-11-18 09:39:29 -0300

    Fix fix-scroll values and add simple comments to it

--- scripts/fix-scroll.sh
+++ scripts/fix-scroll.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-synclient VertScrollDelta=-58
-synclient HorizScrollDelta=-58
+# Watch what number you should put in this with the commands `synclient`
+# Also this file should be put in /etc/init.d/
+synclient VertScrollDelta=-77
+synclient HorizScrollDelta=-77
