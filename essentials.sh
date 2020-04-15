2dd5fc749259ff7891255bf4f0ffc392b31fc58a
Julian Merida <julianmr97@gmail.com>
2020-04-15 13:10:44 -0300

    Add commands to essential

--- essentials.sh
+++ essentials.sh
@@ -1,1 +1,10 @@
-sudo apt install -y vim vim-gtk3 git zsh openssh-client snapd
+sudo apt install -y vim vim-gtk3 git zsh openssh-client snapd vsftpd evince gparted htop xclip
+
+sudo ./oh-my-zsh/zsh.sh
+sudo ./oh-my-zsh/oh-my-zsh.sh
+sudo ./oh-my-zsh/autosuggestion.sh
+
+sudo ./scripts/gitlola.sh
+sudo ./scripts/keyboard-us-altgr-variant.sh
+
+sudo cp rcFiles/vimrc ~/.vimrc && sudo cp rcFiles/zshrc ~/.zshrc
