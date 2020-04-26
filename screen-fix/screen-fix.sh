39e25d60e4fc85f0da84c77d3e1e0a24c4a1740e
Julian Mérida <julianmr97@gmail.com>
2020-04-26 17:59:59 -0300

    Add script to fix HDMI screen

---
+++ screen-fix/screen-fix.sh
@@ -0,0 +1,4 @@
+xrandr --output HDMI-0 --set underscan on
+xrandr --output HDMI-0 --set "underscan hborder" 33 --set "underscan vborder" 21
+
+# xrandr --output HDMI-0 --panning 1280x720 --transform 1.05,0,-30,0,1.05,-20,0,0,1
