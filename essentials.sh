da12ca4a4ca669d7abe2fbced529daaf8acad3ec
Julian Mérida <julianmr97@gmail.com>
2020-05-01 03:43:38 -0300

    Add comments and install vim-plugin command to essentials

--- essentials.sh
+++ essentials.sh
@@ -1,10 +1,20 @@
+# Install fundamental programs
 sudo apt install -y vim vim-gtk3 git zsh openssh-client snapd vsftpd evince gparted htop xclip
 
+# Install and set zsh
 ./oh-my-zsh/zsh.sh
 ./oh-my-zsh/oh-my-zsh.sh
 ./oh-my-zsh/autosuggestion.sh
 
+# Set permanent shortcuts
 ./scripts/gitlola.sh
 ./scripts/keyboard-us-altgr-variant.sh
 
+# Copy rcFiles
 cp rcFiles/vimrc ~/.vimrc && cp rcFiles/zshrc ~/.zshrc
+
+# Install vim vundle
+./install-vundle/install.sh
+
+# Install all plugins
+vim +PluginInstall +qall
