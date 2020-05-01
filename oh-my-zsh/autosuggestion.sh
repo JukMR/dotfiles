a8be2a776d62471c999da4cff25775e6355c4faf
Julian Mérida <julianmr97@gmail.com>
2020-05-01 02:48:34 -0300

    Add BUFFER_MAX_SIZE to autosuggestion

--- oh-my-zsh/autosuggestion.sh
+++ oh-my-zsh/autosuggestion.sh
@@ -1,3 +1,4 @@
 cd ~/.oh-my-zsh/custom/plugins &&
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&
-echo ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=\"fg=3,bg=cyan,bold,underline\" > ~/.oh-my-zsh/custom/autosuggetion.zsh
+echo ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=\"fg=00,bg=cyan,bold,underline\" > ~/.oh-my-zsh/custom/autosuggetion.zsh &&
+echo ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20 > /.oh-my-zsh/custom/autosuggetion.zsh
