58182032f7deafdac27e3d66b935a2a200589c2d
Julian Mérida <julianmr97@gmail.com>
2020-05-14 19:08:23 -0300

    Add vokoscreen install instructions

---
+++ vokoscreen/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Step 1:
+sudo add-apt-repository ppa:ubuntuhandbook1/apps && 
+
+# Step 2:
+sudo apt update && sudo apt install vokoscreen-ng &&
+
+# Step 3:
+
+sudo apt update && sudo apt install ubuntu-restricted-extras
