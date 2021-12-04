01fba7e6ee40cc2a5d78b7c9a45932f11e59cfdd
Julian Merida <julianmr97@gmail.com>
2021-12-04 17:25:23 -0300

    Improve manjaro_essential autoinstall script

--- manjaro_essentials.sh
+++ manjaro_essentials.sh
@@ -6,8 +6,8 @@ sudo pacman -Syu --noconfirm --needed git zsh openssh vsftpd evince gparted htop
 
 # Install and set zsh
 $dotdir/programs/oh-my-zsh/zsh.sh &&
-$dotdir/programs/oh-my-zsh/oh-my-zsh.sh ;
-$dotdir/dotfiles/programs/oh-my-zsh/autosuggestion.sh ;
+$dotdir/programs/oh-my-zsh/oh-my-zsh-unattended.sh &&
+$dotdir/programs/oh-my-zsh/autosuggestion.sh ;
 
 # Set permanent shortcuts
 $dotdir/scripts/gitlola.sh &&
@@ -18,7 +18,12 @@ cp $dotdir/rcFiles/vimrc ~/.vimrc &&
 cp $dotdir/rcFiles/zshrc ~/.zshrc &&
 
 # Install vim vundle
-$dotdir/programs/install-vundle/install.sh &&
+$dotdir/programs/install-vundle/install.sh ;
 
 # Install all plugins
-vim +PluginInstall +qall
+vim +PluginInstall +qall &&
+
+# Install kitty terminal
+mkdir -p $HOME/.local/bin &&
+mkdir -p $HOME/.local/share/applications &&
+$dotdir/programs/kitty/install.sh
