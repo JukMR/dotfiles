ddf8ff9781c72d3b20277290b25e47e770a0ee93
Julian Merida <julianmr97@gmail.com>
2023-02-08 08:48:54 -0300

    Main_script: copy kitty.conf and resolve some issues

--- manjaro_essentials.sh
+++ manjaro_essentials.sh
@@ -43,6 +43,7 @@ $dotdir/scripts/keyboard-us-altgr-variant.sh &&
 
 # Copy rcFiles
 cp $dotdir/rcFiles/vimrc ~/.vimrc &&
+cp ~/.zshrc ~/.old_zshrc &&
 cp $dotdir/rcFiles/zshrc ~/.zshrc &&
 
 # Install vim vundle
@@ -66,10 +67,13 @@ fi
 mkdir -p $HOME/.config/awesome &&
 cp -ur $dotdir/programs/awesome $HOME/.config
 
+# Move kitty config
+mkdir -p $HOME/.config/kitty &&
+cp $dotdir/programs/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+
 # Configure git name and email
 git config --global user.name "Julian Merida"
 git config --global user.email "julianmr97@gmail.com"
-
 
 # Clone awesome plugin repositories
 cd $HOME/.config/awesome
@@ -80,11 +84,14 @@ git clone https://github.com/pltanton/net_widgets.git
 $dotdir/scripts/create_cronjob.sh
 
 # Pamac installation packages
-pamac_programs="
-mirage
-visual-studio-code-bin
-spotify
-"
+# Find a way to install programs if one wasn't found
+# pamac_programs="
+# mirage
+# visual-studio-code-bin
+# spotify
+# "
 
-pamac install $pamac_programs
+pamac install visual-studio-code-bin
+pamac install spotify
+pamac install mirage
 
