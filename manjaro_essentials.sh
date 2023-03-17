59bdee928d473ab117fc502534aa707441f157d5
Julian Merida <julianmr97@gmail.com>
2023-03-17 00:32:31 -0300

    Manjaro_essentials: pass shellcheck

--- manjaro_essentials.sh
+++ manjaro_essentials.sh
@@ -32,24 +32,24 @@ viewnior
 brave-browser
 xlockmore
 "
-sudo pacman -Syu --noconfirm --needed $programs &&
+sudo pacman -Syu --noconfirm --needed "$programs" &&
 
 # Install and set zsh
-$dotdir/programs/oh-my-zsh/zsh.sh &&
-$dotdir/programs/oh-my-zsh/oh-my-zsh-unattended.sh &&
-$dotdir/programs/oh-my-zsh/autosuggestion.sh ;
+"$dotdir"/programs/oh-my-zsh/zsh.sh &&
+"$dotdir"/programs/oh-my-zsh/oh-my-zsh-unattended.sh &&
+"$dotdir"/programs/oh-my-zsh/autosuggestion.sh ;
 
 # Set permanent shortcuts
-$dotdir/scripts/gitlola.sh &&
-$dotdir/scripts/keyboard-us-altgr-variant.sh &&
+"$dotdir"/scripts/gitlola.sh &&
+"$dotdir"/scripts/keyboard-us-altgr-variant.sh &&
 
 # Copy rcFiles
-cp $dotdir/rcFiles/vimrc ~/.vimrc &&
+cp "$dotdir"/rcFiles/vimrc ~/.vimrc &&
 cp ~/.zshrc ~/.old_zshrc &&
-cp $dotdir/rcFiles/zshrc ~/.zshrc &&
+cp "$dotdir"/rcFiles/zshrc ~/.zshrc &&
 
 # Install vim vundle
-$dotdir/programs/install-vundle/install.sh ;
+"$dotdir"/programs/install-vundle/install.sh ;
 
 # Install all plugins
 vim +PluginInstall +qall &&
@@ -57,34 +57,34 @@ vim +PluginInstall +qall &&
 # Install kitty terminal
 # mkdir -p $HOME/.local/bin &&
 # mkdir -p $HOME/.local/share/applications &&
-# $dotdir/programs/kitty/install.sh
+# "$dotdir"/programs/kitty/install.sh
 
 # Change default shell to zsh
 if [ "$SHELL" != "/bin/zsh" ]
 then
-  chsh -s $(which zsh)
+  chsh -s "$(which zsh)"
 fi
 
 # Move awesome rc.lua config
-mkdir -p $HOME/.config/awesome &&
-cp -ur $dotdir/programs/awesome $HOME/.config
+mkdir -p "$HOME"/.config/awesome &&
+cp -ur "$dotdir"/programs/awesome "$HOME"/.config
 
 # Move kitty config
-mkdir -p $HOME/.config/kitty &&
-cp $dotdir/programs/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+mkdir -p "$HOME"/.config/kitty &&
+cp "$dotdir"/programs/kitty/kitty.conf "$HOME"/.config/kitty/kitty.conf
 
 # Configure git name and email
 git config --global user.name "Julian Merida"
 git config --global user.email "julianmr97@gmail.com"
 
 # Clone awesome plugin repositories
-cd $HOME/.config/awesome
+cd "$HOME"/.config/awesome || exit
 git clone https://github.com/streetturtle/awesome-wm-widgets
 git clone https://github.com/pltanton/net_widgets.git
 
 # Initiate cronjob wallpaper changer script
-mkdir -p $HOME/Pictures/wallpapers
-$dotdir/scripts/create_cronjob.sh
+mkdir -p "$HOME"/Pictures/wallpapers
+"$dotdir"/scripts/create_cronjob.sh
 
 # Pamac installation packages
 # Find a way to install programs if one wasn't found
