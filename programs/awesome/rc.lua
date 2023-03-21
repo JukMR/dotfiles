ae2acef22ac5feca841a81233335bde91edfd241
Julian Merida <julianmr97@gmail.com>
2023-03-21 14:20:43 -0300

    Awesome: change default config of wifi and battery to true

--- programs/awesome/rc.lua
+++ programs/awesome/rc.lua
@@ -2,8 +2,8 @@
 
 -- Global config variables
 
-battery = false
-wifi = false
+battery = true
+wifi = true
 
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
