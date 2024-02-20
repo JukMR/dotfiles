454e8b316639f86cc35bb5bd49b5e577aba95d67
Julian Merida <julianmr97@gmail.com>
2024-02-20 20:35:32 -0300

    program: add atuin scripts

---
+++ programs/atuin/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# bash/zsh/etc
+bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
---
+++ programs/atuin/login.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+atuin register -u jukmr -e julianmr97@gmail.com
+atuin import auto
+atuin sync
