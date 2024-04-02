62d03eff038c42fdd91c386db082e54f64402b1f
Julian Merida <julianmr97@gmail.com>
2024-04-02 01:50:47 -0300

    programs: add zoxide

---
+++ programs/zoxide/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo 'Installing zoxide'
+
+# Check if zoxide is already installed
+if command -v zoxide &>/dev/null; then
+    echo 'zoxide is already installed'
+else
+    echo 'Installing zoxide'
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+    echo 'zoxide installed'
+fi
+
+# Check if zoxide is already added to .zshrc, if not add it
+# eval "$(zoxide init zsh)" is the command that needs to be added to .zshrc
+if ! grep -q "zoxide init zsh" ~/.zshrc; then
+    # shellcheck disable=SC2016
+    echo 'eval "$(zoxide init zsh)"' >>~/.zshrc
+    echo 'zoxide directive added to ~/.zshrc'
+else
+    echo 'zoxide directive already added to ~/.zshrc'
+
+fi
