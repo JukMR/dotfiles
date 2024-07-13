63b93bb76ebc566c8d3f71e29d564cc7c7832e96
Julian Merida <julianmr97@gmail.com>
2024-07-13 20:48:24 -0300

    manjaro_essentials: improve commments and code style

--- manjaro_essentials.sh
+++ manjaro_essentials.sh
@@ -4,12 +4,6 @@
 # Author: Julian Merida
 # Last Updated: $(date +%Y-%m-%d)
 
-# Check for root
-#   if [[ $EUID -ne 0 ]]; then
-#       echo "This script must be run as root"
-#       exit 1
-#   fi
-
 dotdir="$HOME/dotfiles"
 
 # Enable strict mode
@@ -19,7 +13,8 @@ set -eu
 exec > >(tee -i "$HOME"/dotfiles_setup.log)
 exec 2>&1
 
-# Install kitty terminal
+### ---------- kitty -------------------- ###
+
 # Check if kitty is installed
 if ! command -v kitty &>/dev/null; then
     echo "Installing kitty terminal"
@@ -28,10 +23,11 @@ else
     echo "Kitty terminal is already installed."
 fi
 
-# Configure pacman
+### ---------- Configure pacman ------------------- ###
 sudo bash "$dotdir"/programs/pacman/configure_pacman.sh
 
-# Install yay
+### ---------- yay ------------------- ###
+
 # Check if yay is installed
 if ! command -v yay &>/dev/null; then
     echo "Installing yay"
@@ -40,7 +36,8 @@ else
     echo "Yay is already installed."
 fi
 
-# Install fundamental programs
+### ---------- essential programs -------------------- ###
+
 programs="
 git
 zsh
@@ -106,6 +103,8 @@ else
     echo "All specified programs are already installed."
 fi
 
+### ---------- zsh -------------------- ###
+
 # Install and set zsh
 if [ ! -d ~/.oh-my-zsh ] || [ ! -f ~/.zshrc ]; then
     # If either the .oh-my-zsh directory or the .zshrc file doesn't exist, run the installation scripts
@@ -114,16 +113,22 @@ if [ ! -d ~/.oh-my-zsh ] || [ ! -f ~/.zshrc ]; then
     "$dotdir"/programs/oh-my-zsh/autosuggestion.sh
 fi
 
+### ---------- vim-like-mode -------------------- ###
+
 # Install vim-like-mode plugin for zsh
 if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-vim-mode ]; then
     "$dotdir"/programs/oh-my-zsh/vim-like-mode/vim-like-mode.sh
 fi
 
+### ---------- permanent shortcuts -------------------- ###
+
 # Set permanent shortcuts
 "$dotdir"/scripts/gitlola.sh
 "$dotdir"/scripts/keyboard-us-altgr-variant.sh
 
-# Backup and copy rcFiles
+### ---------- zshrc -------------------- ###
+
+# Backup and copy zshrc
 cp -v ~/.zshrc ~/.old_zshrc
 cp -v "$dotdir"/rcFiles/zshrc ~/.zshrc
 
@@ -139,9 +144,13 @@ fi
 grep -q 'source $HOME/.oh-my-zsh/custom/plugins/zsh-vim-mode/zsh-vim-mode.plugin.zsh' "$HOME/.zshrc" ||
     echo 'source $HOME/.oh-my-zsh/custom/plugins/zsh-vim-mode/zsh-vim-mode.plugin.zsh' >>"$HOME/.zshrc"
 
+### ---------- awesome rc.lua -------------------- ###
+
 # Backup and move awesome rc.lua config
 mkdir -pv "$HOME"/.config/awesome
 cp -uvr "$dotdir"/programs/awesome "$HOME"/.config
+
+### ---------- kitty config -------------------- ###
 
 # Backup and move kitty config
 KITTY_CONF_FOLDER="$HOME/.config/kitty"
@@ -150,6 +159,8 @@ KITTY_CONF_FILE="$KITTY_CONF_FOLDER/kitty.conf"
 mkdir -pv KITTY_CONF_FOLDER
 cp -v "$KITTY_CONF_FILE" "$HOME"/.config/kitty/kitty_bkp.conf || echo 'Failed to copy default kitty.conf from config. Posibly it doesnt exists'
 cp -v "$dotdir"/programs/kitty/kitty.conf "$KITTY_CONF_FILE"
+
+### ---------- configure git -------------------- ###
 
 # Configure git name and email
 GIT_NAME="Julian Merida"
@@ -165,8 +176,11 @@ git config --global alias.final-branches '!git for-each-ref --format="%(objectna
 # Add git undo alias
 git config --global alias.undo 'reset --soft HEAD^'
 
-# Install git-diff-image
+### ---------- install git-diff-image -------------------- ###
+
 bash "$dotdir"/programs/git-diff-image/install.sh
+
+### ---------- awesome plugins -------------------- ###
 
 # Clone awesome plugin repositories
 if [ ! -d "$HOME/.config/awesome/awesome-wm-widgets" ]; then
@@ -184,21 +198,25 @@ echo "Initiating cronjob wallpaper changer script"
 mkdir -pv "$HOME"/Pictures/wallpapers
 "$dotdir"/scripts/create_cronjob.sh
 
-# Install astrovim
-echo "Installing astrovim"
-bash "$dotdir"/programs/neovim/astrovim/install.sh
+### ---------- astronvim -------------------- ###
+
+# Install astronvim
+echo "Installing astronvim"
+bash "$dotdir"/programs/neovim/astronvim/install.sh
 
 # Copy config nvim repo
 echo "Copying nvim config"
-mkdir -pv ~/.config/nvim/lua/user
-git clone https://github.com/JuKMR/nvim_plugins ~/.config/nvim/lua/user
+bash "$dotdir"/programs/neovim/astronvim/setup_custom_configuration.sh
+
+### ---------- pamac configuration -------------------- ###
 
 # Enable AUR in pamac.conf
-
 sudo sed -i 's/#EnableAUR/EnableAUR/' /etc/pamac.conf
 
 # Pamac installation packages
 yay -S --noconfirm visual-studio-code-bin
+
+### ---------- atuin -------------------- ###
 
 # Install atuin and register
 echo "Installing atuin"
@@ -208,15 +226,16 @@ echo "Login in into atuin"
 # NOTE: disabling this until we fix how to login correctly
 # bash "$dotdir"/programs/atuin/login.sh
 
-# Disabling this for now
 echo "Enable ctrl_n_shortcuts in autin"
 bash "$dotdir"/programs/atuin/apply_config.sh
 
-# Copy picom files
+### ---------- picom -------------------- ###
 
 echo "Copying picom files"
 mkdir -pv "$HOME"/.config/picom
 cp -v "$dotdir"/programs/picom/picom.conf "$HOME"/.config/picom/picom.conf
+
+### ---------- zoxide -------------------- ###
 
 # Installing zoxide
 echo "Installing zoxide"
