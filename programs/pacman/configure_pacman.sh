f4342c285b1f320ed105f195af375e257c9492bc
Julian Merida <julianmr97@gmail.com>
2024-07-13 20:26:17 -0300

    program: add script to configure_pacman automatically

---
+++ programs/pacman/configure_pacman.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+# Script to set up a new Linux environment
+
+# Set colors option in pacman
+sed -i -E -e 's/# *Color/Color/g' /etc/pacman.conf
+
+# # Enable parallel downloads
+sed -i -E -e 's/# *ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+
+# Enable ILoveCandy
+
+# Check if ILoveCandy is enabled
+# If it is not enabled, enable it adding the line under ParallelDownloads
+
+if ! grep -q "ILoveCandy" /etc/pacman.conf; then
+    sed -i -e '/ParallelDownloads = 5/a ILoveCandy' /etc/pacman.conf
+fi
