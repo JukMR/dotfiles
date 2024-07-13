6fe686dd4ca1c635961e0409a69c469434c7f273
Julian Merida <julianmr97@gmail.com>
2024-07-13 20:05:22 -0300

    astronvim: improve nvim configuration setup

--- programs/neovim/astrovim/plugins/add_suda_write_plugin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Doing backup of user.lua file"
-cp -v /home/mrjulian/.config/nvim/lua/plugins/user.lua /home/mrjulian/.config/nvim/lua/plugins/user.lua.bkp
-
-echo "Copying new user.lua file"
-cp -v user.lua /home/mrjulian/.config/nvim/lua/plugins/user.lua
-
-
---
+++ programs/neovim/astrovim/setup_custom_configuration.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+echo "Cloning and setting nvim configuration"
+git clone https://github.com/JuKMR/astronvim-configuration ~/.config/nvim/
