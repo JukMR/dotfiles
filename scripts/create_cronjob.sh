7d6a833d2b9d6d1ae526a37b4fc64a372244db0a
Julian Merida <julianmr97@gmail.com>
2024-07-14 23:13:12 -0300

    scripts: update create_cronjob.sh :

--- scripts/create_cronjob.sh
+++ scripts/create_cronjob.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-OUT="$(mktemp /tmp/output.XXXXXXXXXX)" || {
-    echo "Failed to create temp file"
-    exit 1
-}
 
-crontab -l >"$OUT"
+set -eu
 
-echo "*/10 * * * *  $HOME/dotfiles/scripts/wallpaper_changer_cron.sh" >>"$OUT"
+echo "*/10 * * * *  $HOME/dotfiles/scripts/wallpaper_changer_cron.sh" > wallpaper_set.cron
 
-crontab "$OUT"
+crontab wallpaper_set.cron
 
 echo "Cronjob started correctly:"
-
 crontab -l
 
-echo "Check if the $HOME/Pictures/wallpapers" folder exists. Put wallpapers there.
+echo "Create $HOME/Pictures/wallpapers folder. Put wallpapers there"
+mkdir -p "$HOME/Pictures/wallpapers"
