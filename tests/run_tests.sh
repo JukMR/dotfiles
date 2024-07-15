f2db408e0edd900cce8cc7232daf2ccbc9ef4318
Julian Merida <julianmr97@gmail.com>
2024-07-15 01:29:12 -0300

    tests: add linting test for awesome/rc.lua

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,14 @@
-#/bin/bash
+#!/bin/bash
+
+set -eu
+
+DOTDIR="$HOME/dotfiles"
 
 echo 'Running shellcheck'
 shellcheck -S style ../manjaro_essentials.sh
+
+echo 'Run checks for awesome/rc.lua'
+luacheck "$DOTDIR"/programs/awesome/rc.lua
 
 echo 'Running docker test'
 docker build -t test-environment .
