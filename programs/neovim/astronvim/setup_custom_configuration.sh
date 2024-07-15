c6fa54b1ec96ec6ada936ba4d160e1ae209c7754
Julian Merida <julianmr97@gmail.com>
2024-07-14 23:23:25 -0300

    programs: improve astronvim configuration script

--- programs/neovim/astronvim/setup_custom_configuration.sh
+++ programs/neovim/astronvim/setup_custom_configuration.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 
+# Backup files
+mv -v ~/.config/nvim ~/.config/nvim.bak
+mv -v ~/.local/share/nvim ~/.local/share/nvim.bak
+mv -v ~/.local/state/nvim ~/.local/state/nvim.bak
+mv -v ~/.cache/nvim ~/.cache/nvim.bak
+
 echo "Cloning and setting nvim configuration"
 git clone https://github.com/JuKMR/astronvim-configuration ~/.config/nvim/
