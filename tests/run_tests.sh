219ed03caad93c6f00d49283d3eaac050b4cc8dc
Julian Merida <julianmr97@gmail.com>
2024-07-19 00:58:05 -0300

    tests: make luacheck only check for error messages

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,7 +8,7 @@ echo 'Running shellcheck'
 shellcheck -S style ../manjaro_essentials.sh
 
 echo 'Run checks for awesome/rc.lua'
-luacheck "$DOTDIR"/programs/awesome/rc.lua
+luacheck "$DOTDIR"/programs/awesome/rc.lua --only 0
 
 echo 'Running docker test'
 docker build -t test-environment .
