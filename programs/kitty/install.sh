95be507ead42693e0c223088e121c43a42954d04
Julian Merida <julianmr97@gmail.com>
2024-10-05 14:52:15 -0300

    Create ~/.local/bin if it doesn't exists

--- programs/kitty/install.sh
+++ programs/kitty/install.sh
@@ -14,6 +14,7 @@ curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 # Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
 # your system-wide PATH)
 echo "Adding kitty to PATH..."
+mkdir -p ~/.local/bin/
 ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
 
 # Place the kitty.desktop file somewhere it can be found by the OS
