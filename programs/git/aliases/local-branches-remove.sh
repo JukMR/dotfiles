ea3d7b5f435f7d7d9f737185b51e37e0c7a25cf9
Julian Merida <julianmr97@gmail.com>
2025-01-30 17:28:00 -0300

    git aliases: fix git alias wrongly named

--- programs/git/aliases/local-branches-remove.sh
+++ programs/git/aliases/local-branches-remove.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git config --global alias.local-branches-show "!git branch -v | grep '\\[gone\\]' | awk '{print \$1}' | xargs git branch -D"
+git config --global alias.local-branches-remove "!git branch -v | grep '\\[gone\\]' | awk '{print \$1}' | xargs git branch -D"
