8afc6cca85a3d9843f19df58e9f9cef6e862a41a
Julian Merida <julianmr97@gmail.com>
2026-04-20 19:06:23 -0300

    programs: add watch-your-lan docker

---
+++ programs/watch-your-lan/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run --name wyl \
+	-e "IFACES=$YOURIFACE" \
+	-e "TZ=$YOURTIMEZONE" \
+	--network="host" \
+	-v $DOCKERDATAPATH/wyl:/data/WatchYourLAN \
+    aceberg/watchyourlan
